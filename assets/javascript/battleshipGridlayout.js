ecca8d42dc2445601a2f43812df58880ec4d3b69
e30devo <dthomase30@gmail.com>
2018-01-13 13:36:05 -0800

    Adding battleshitGridlayout.js

---
+++ assets/javascript/battleshipGridlayout.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+
+var xAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var yAxis = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+
+var playerGrid = (xAxis.length * yAxis.length);
+
+console.log(playerGrid);
+
+}); //document.ready close
