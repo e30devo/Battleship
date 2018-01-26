61c618d207c46b9776e8911dd3ebc0a6c92249d7
Jeremy Gruhlkey <jeremygruhlkey@Naeuns-MacBook-Pro.local>
2018-01-25 17:41:51 -0800

    fixed dom update for opponent name

--- assets/javascript/pairing.js
+++ assets/javascript/pairing.js
@@ -485,10 +485,10 @@ $(document).ready(function() {
 	database.ref().on("value", function(snapshot) {
 		winExists = snapshot.child(myGame + "/win/").exists();
 		gameWinner = snapshot.child(myGame +"/win/").val();
-		oppopnentName = snapshot.child(opPath + "email").val();
+		opponentName = snapshot.child(opPath + "email/").val();
+		$("#op-email").html(opponentName);
 		if (winExists) {
 			$("#notification").html("[ The Winner is " + gameWinner + " ]");
-			// $("#op-email").html(opponentName);
 			$(document).off("click");			
 		}
 	})
