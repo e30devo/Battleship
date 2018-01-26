c4a716f8ecc329e29fe4737a2a743b34d927ec60
Nema Darban <nema.darban@gmail.com>
2018-01-25 16:42:45 -0800

    fixed float length

--- assets/javascript/playerRanking.js
+++ assets/javascript/playerRanking.js
@@ -29,12 +29,10 @@ var output = {};
 
 	database.ref("/users").on("value", function(snapshot) {
 		snapshot.forEach(function(child) {
-			/*console.log(child.child("average").val());*/
 			var email = child.child("email").val();
 			var average = child.child("average").val();
 			stats[email] = average;
 		});
-		/*console.log(stats);*/
 
 	})
 
@@ -42,24 +40,19 @@ var output = {};
 	function sort() {
 		$(".playerRanking").empty();
 		ranked = Object.keys(stats).sort(function(a,b){return stats[b]-stats[a]});
-		$(".playerRanking").append("<thead><tr><th>Rank</th><th>Username</th><th>Win/Loss Average</th></tr></thead><tbody class='playerRankingTable'></tbody>");
+		$(".playerRanking").append(
+			"<thead><tr><th>Rank</th><th>Username</th><th>Win/Loss Average</th></tr></thead><tbody class='playerRankingTable'></tbody>");
 		for (i = 0; i < ranked.length; i++) {
 			var obj = ranked[i]; //gets email
-
-	/*		if (stats[obj]){
-				output[obj] = stats[obj]; //assigns average to the user in object	
-			} else {
-				output[obj] = 0; //if average is null give it a zero
-			}*/
 
 			var place = i+1;
 			if (stats[obj] === null) {
 				stats[obj] = 0;
 			}
-			$(".playerRankingTable").append("<tr><td>"+place+"</td><td>"+ranked[i]+"</td><td>"+stats[obj]+"</tr>");
+
+			$(".playerRankingTable").append("<tr><td>"+place+"</td><td>"+ranked[i]+"</td><td>"+stats[obj].toFixed(4)+"</tr>");
 		}
 	
-		console.log(output);
 		
 	}
 
